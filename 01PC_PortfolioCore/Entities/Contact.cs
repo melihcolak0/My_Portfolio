c85d676a6af85f73a499be64e0e7490a2e383065
Melih Çolak <melihcolak0@gmail.com>
2025-03-28 22:11:12 +0300

    Add project files.

---
+++ 01PC_PortfolioCore/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace _01PC_PortfolioCore.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Location { get; set; }
+    }
+}
---
+++ 01PC_PortfolioCore/Entities/Education.cs
@@ -0,0 +1,15 @@
+﻿namespace _01PC_PortfolioCore.Entities
+{
+    public class Education
+    {
+        public int EducationId { get; set; }
+
+        public string Title { get; set; }
+
+        public string DateInfo { get; set; }
+
+        public string  SubTitle { get; set; }
+
+        public string Description { get; set; }
+    }
+}
