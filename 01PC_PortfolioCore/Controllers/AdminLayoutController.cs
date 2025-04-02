c2058ea954311473f1cb3544bf2a2accb9c131e6
Melih Çolak <melihcolak0@gmail.com>
2025-04-02 22:00:11 +0300

    ...

---
+++ 01PC_PortfolioCore/Controllers/AdminLayoutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01PC_PortfolioCore.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartail()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SideBarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+    }
+}
